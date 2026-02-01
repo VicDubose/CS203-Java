573642665bf9e7134cc220c24f95ccfeeafe9413
Victor Dubose <Tyr1jr1@uab.edu>
2026-02-01 10:06:09 -0600

    init: first java test

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("✅ VS Code Java run test (WSL)");
+        System.out.println("Repo: CS203-Java");
+    }
+}
